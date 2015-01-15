498c8d1a0fd71c122dfd1f65cf78391677349390
Paul Bovbel <pbovbel@clearpathrobotics.com>
2015-01-15 12:03:52 -0500

    Refactor to allow debug messages from node and nodelet

--- pointcloud_to_laserscan/src/pointcloud_to_laserscan_node.cpp
+++ pointcloud_to_laserscan/src/pointcloud_to_laserscan_node.cpp
@@ -39,24 +39,27 @@
  */
 
 #include <ros/ros.h>
-#include <pointcloud_to_laserscan/pointcloud_to_laserscan_base.h>
+#include <nodelet/loader.h>
 
 int main(int argc, char **argv){
-    ros::init(argc, argv, "pointcloud_to_laserscan");
-    ros::NodeHandle nh;
-    ros::NodeHandle private_nh("~");
-    bool use_concurrency;
+  ros::init(argc, argv, "pointcloud_to_laserscan_node");
+  ros::NodeHandle private_nh("~");
+  int concurrency_level;
+  private_nh.param<int>("concurrency_level", concurrency_level, 0);
 
-    private_nh.param<bool>("use_concurrency", use_concurrency, false);
+  nodelet::Loader nodelet;
+  nodelet::M_string remap(ros::names::getRemappings());
+  nodelet::V_string nargv;
+  std::string nodelet_name = ros::this_node::getName();
+  nodelet.load(nodelet_name, "pointcloud_to_laserscan/pointcloud_to_laserscan_nodelet", remap, nargv);
 
-    pointcloud_to_laserscan::PointCloudToLaserScanBase node(nh, private_nh);
+  boost::shared_ptr<ros::MultiThreadedSpinner> spinner;
+  if(concurrency_level) {
+    spinner.reset(new ros::MultiThreadedSpinner(concurrency_level));
+  }else{
+    spinner.reset(new ros::MultiThreadedSpinner());
+  }
+  spinner->spin();
+  return 0;
 
-    if(use_concurrency) {
-        ros::MultiThreadedSpinner spinner;
-        spinner.spin();
-    }else{
-        ros::spin();
-    }
-
-    return 0;
 }
