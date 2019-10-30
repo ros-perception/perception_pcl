14ffd6fd0d42a4cdb8d8d01d510bac70974a10c2
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2019-10-30 10:11:41 +0100

    Fix output error, improve formatting

--- pcl_ros/tools/pointcloud_to_pcd.cpp
+++ pcl_ros/tools/pointcloud_to_pcd.cpp
@@ -116,20 +116,20 @@ class PointCloudToPCD
 
       pcl::PCDWriter writer;
       if(binary_)
-	{
-	  if(compressed_)
-	    {
-	      writer.writeBinaryCompressed (ss.str (), *cloud, v, q);
-	    }
-	  else
-	    {
-	      writer.writeBinary (ss.str (), *cloud, v, q);
-	    }
-	}
+      {
+        if(compressed_)
+        {
+          writer.writeBinaryCompressed (ss.str (), *cloud, v, q);
+        }
+        else
+        {
+          writer.writeBinary (ss.str (), *cloud, v, q);
+        }
+      }
       else
-	{
-	  writer.writeASCII (ss.str (), *cloud, v, q, 8);
-	}
+      {
+        writer.writeASCII (ss.str (), *cloud, v, q, 8);
+      }
 
     }
 
@@ -153,20 +153,20 @@ class PointCloudToPCD
       priv_nh.getParam ("compressed", compressed_);
       priv_nh.getParam ("filename", filename_);
       if(binary_)
-	{
-	  if(compressed_)
-	    {
-	      ROS_INFO_STREAM ("Saving as binary compressed PCD");
-	    }
-	  else
-	    {
-	      ROS_INFO_STREAM ("Saving as binary PCD");
-	    }
-	}
+      {
+        if(compressed_)
+        {
+          ROS_INFO_STREAM ("Saving as binary compressed PCD");
+        }
+        else
+        {
+          ROS_INFO_STREAM ("Saving as binary uncompressed PCD");
+        }
+      }
       else
-	{
-	  ROS_INFO_STREAM ("Saving as binary PCD");
-	}
+      {
+        ROS_INFO_STREAM ("Saving as ASCII PCD");
+      }
 
       if (filename_ != "")
       {
