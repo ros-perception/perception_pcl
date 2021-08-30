1a214ccc6eea62c57bf359c6dac50452e0257ce4
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-08-31 03:08:25 +0900

    Fix obsolete header (#342)
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- pcl_ros/src/transforms.cpp
+++ pcl_ros/src/transforms.cpp
@@ -43,7 +43,7 @@
 #include <tf2/exceptions.h>
 #include <tf2/LinearMath/Transform.h>
 #include <tf2/LinearMath/Vector3.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 #include <sensor_msgs/msg/point_cloud2.hpp>
