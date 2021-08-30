2d648d13532d5ec6c8ae9c24b4a64460b51dee38
Kenji Miyake <kenji.miyake@tier4.jp>
2021-08-30 09:39:11 +0900

    Fix obsolete header
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- pcl_ros/include/pcl_ros/impl/transforms.hpp
+++ pcl_ros/include/pcl_ros/impl/transforms.hpp
@@ -45,7 +45,7 @@
 #include <tf2/LinearMath/Transform.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Vector3.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/msg/transform_stamped.hpp>
