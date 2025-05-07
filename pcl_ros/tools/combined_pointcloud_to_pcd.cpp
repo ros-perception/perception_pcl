cfb47f51d685a546ac59b25f21a06fe7ef766ca3
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2025-05-07 15:52:14 +0200

    Fix copyright notice

--- pcl_ros/tools/combined_pointcloud_to_pcd.cpp
+++ pcl_ros/tools/combined_pointcloud_to_pcd.cpp
@@ -1,8 +1,7 @@
 /*
  * Software License Agreement (BSD License)
  *
- * Copyright (c) 2009, Willow Garage, Inc.
- * Copyright (c) 2025, Valerio Passamano <val1212.pass@gmail.com>
+ *  Copyright (c) 2009, Willow Garage, Inc.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -15,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of Valerio Passamano. nor the names of its
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -27,7 +26,7 @@
  *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
  *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
  *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
