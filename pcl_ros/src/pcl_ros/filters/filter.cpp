92d1efce7c1cdf699cc63b8fdac8c810c958a95b
Hadrien <hdu@lescompanions.fr>
2026-02-13 11:03:28 +0100

    pcl_ros: use sub_options in message_filters subscription

--- pcl_ros/src/pcl_ros/filters/filter.cpp
+++ pcl_ros/src/pcl_ros/filters/filter.cpp
@@ -134,8 +134,8 @@ pcl_ros::Filter::subscribe()
   if (use_indices_) {
     // Subscribe to the input using a filter
     auto sensor_qos_profile = rclcpp::SensorDataQoS().keep_last(max_queue_size_);
-    sub_input_filter_.subscribe(this, "input", sensor_qos_profile);
-    sub_indices_filter_.subscribe(this, "indices", sensor_qos_profile);
+    sub_input_filter_.subscribe(this, "input", sensor_qos_profile, sub_options);
+    sub_indices_filter_.subscribe(this, "indices", sensor_qos_profile, sub_options);
 
     if (approximate_sync_) {
       sync_input_indices_a_ =
@@ -165,7 +165,7 @@ pcl_ros::Filter::subscribe()
     sub_input_ =
       this->create_subscription<PointCloud2>(
       "input", rclcpp::SensorDataQoS().keep_last(max_queue_size_),
-      callback);
+      callback, sub_options);
   }
 }
 
